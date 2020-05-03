6b287a489db2a180fc959815564d461087effbfa
Hannah Malcolm <kawaiihannah@gmail.com>
2020-05-04 09:52:35 +1000

    Fixes #1. Bootstrapping and setup

--- plugins/last-fm-api-integration/inc/namespace.php
+++ plugins/last-fm-api-integration/inc/namespace.php
@@ -1,5 +1,5 @@
 <?php
-// Bootstrap the plugin.
+
 namespace Hannah\Last_FM;
 
 /**
@@ -8,5 +8,22 @@ namespace Hannah\Last_FM;
  * @return void
  */
 function bootstrap() {
-	// Add your action and filter hooks inside here.
+	require_once __DIR__ . '../../vendor/cmb2/cmb2/init.php';
+	add_action( 'cmb2_admin_init', __NAMESPACE__ . '\\register_options_page' );
+}
+
+/**
+ * Register the CMB2 options page for capturing necessary data for our plugin
+ * @return void
+ */
+function register_options_page() {
+	$cmb_options = new_cmb2_box(
+		[
+			'id'           => LAST_FM_PREFIX . '_plugin_options_page',
+			'title'        => __( 'LastFM Options', 'last_fm' ),
+			'object_types' => ['options-page'],
+			'option_key'   => LAST_FM_PREFIX . '_plugin_options', // The option key and admin menu page slug.
+			'icon_url'     => 'dashicons-format-audio', // Menu icon. Only applicable if 'parent_slug' is left empty.
+		]
+	);
 }
